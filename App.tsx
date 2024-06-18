28e800155ab2736c9b5a0efcfb7e8872137423ab
Mohsina Parveen <159033605+mohsina21@users.noreply.github.com>
2024-06-18 06:03:25 +0000

    bg

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 
+
+const styles = StyleSheet.create({})
 import React from 'react';
 import {
   SafeAreaView,
@@ -8,15 +10,30 @@ import {
   Text,
   useColorScheme,
   View,
+  useState
+  TouchableOpacity
 } from 'react-native';
 
 
-function App(): JSX.Element {
+function App(): React.JSX.Element {
 
-
+  const [bg,setbg] = useState("#ffffff")
+  const generateColor = ()=>{
+    const hexrange = "0123456789ABCDEF"
+    let color = "#"
+    for (let i = 0; i<6; i++){
+      color += hexrange[Math.floor(Math.random()*16)]
+    }
+  }
   return (
-    <StatusBar> backgroundColor = "#000000" </StatusBar>
-    
+    <><StatusBar> backgroundColor = {"#000000"} </StatusBar><View style={styles.container}>
+      <TouchableOpacity>
+        <View style = {styles.actionBtn}>
+          <Text style = {styles.actionbtntext}> press me</Text>
+        </View>
+
+      </TouchableOpacity>
+    </View></>
   );
 }
 
